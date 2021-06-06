85d1402854458b18135592a37254753df466646f
KangXu <1343450971@qq.com>
2021-06-06 18:28:02 +0800

    1.修改了mvc拦截器配置类

--- src/main/java/com/light/contributionSystem/config/MyMvcConfig.java
+++ src/main/java/com/light/contributionSystem/config/MyMvcConfig.java
@@ -1,6 +1,8 @@
 package com.light.contributionSystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.FormContentFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,10 +14,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/fonts/**", "/js/**", "/plugins/**");
+    }
+
+    /**
+     * @description 接受put和delete请求的
+     **/
+    @Bean
+    public FormContentFilter httpPutFormContentFilter() {
+        return new FormContentFilter();
     }
 }
